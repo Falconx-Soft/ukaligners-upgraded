67f7de6eb60b21df483ba4a5f366362a971def10
unknown <tayyab91929@gmail.com>
2023-03-01 08:51:30 -0800

    Token update

--- Core/settings.py
+++ Core/settings.py
@@ -145,6 +145,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ukaligners@gmail.com'
-EMAIL_HOST_PASSWORD = 'BlueBook&2023'
+EMAIL_HOST_PASSWORD = 'kntjwptntqtuvfna'
 
 
